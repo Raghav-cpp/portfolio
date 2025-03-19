f283de1c273a0519a88ebc9368a400305387244b
Raghav-cpp <84151102+Raghav-cpp@users.noreply.github.com>
2025-03-19 10:14:06 +0000

    404 remove and hashrouter is used instead of browserrouter

--- src/components/About/Socialmedia.js
+++ src/components/About/Socialmedia.js
@@ -4,19 +4,27 @@ import { SiGithub, SiTwitter, SiLinkedin, SiInstagram } from "react-icons/si";
 function Socialmedia() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} href="https://github.com/raghavcpp" className="tech-icons">
-        <SiGithub />
-      </Col>
-      <Col xs={4} md={2} href="https://x.com/Raghavcpp" className="tech-icons">
-        <SiTwitter />
-      </Col>
-      <Col xs={4} md={2} href="https://www.linkedin.com/in/raghav-cpp/" className="tech-icons">
-        <SiLinkedin />
-      </Col>
-      <Col xs={4} md={2} href="https://www.instagram.com/raghav_reincarnation" className="tech-icons">
-        <SiInstagram />
-      </Col>
-    </Row>
+        <Col xs={4} md={2} className="tech-icons">
+            <a href="https://github.com/raghavcpp" target="_blank" rel="noopener noreferrer">
+            <SiGithub />
+            </a>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <a href="https://x.com/Raghavcpp" target="_blank" rel="noopener noreferrer">
+            <SiTwitter />
+            </a>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <a href="https://www.linkedin.com/in/raghav-cpp/" target="_blank" rel="noopener noreferrer">
+            <SiLinkedin />
+            </a>
+        </Col>
+        <Col xs={4} md={2} className="tech-icons">
+            <a href="https://www.instagram.com/raghav_reincarnation" target="_blank" rel="noopener noreferrer">
+            <SiInstagram />
+            </a>
+        </Col>
+    </Row>  
   );
 }
 
